f576492ac5003edf475d0976cadfb6785a6f829c
enamul-java <enamul.java@gmail.com>
2022-10-23 11:25:25 +0600

    Initial commit

---
+++ src/main/java/com/erainfotechbd/cicddemo/demo/CiCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.erainfotechbd.cicddemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdDemoApplication.class, args);
+    }
+
+}
